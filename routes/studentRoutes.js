a62ed6dfbc6c9610c36008d60a461e3da6b7ef2d
sameepabadhuge <sithusameepa@gmail.com>
2025-11-24 23:21:25 +0530

    update student routes for quiz submit backend

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerStudent, loginStudent } from "../controllers/studentController.js";
+import { registerStudent, loginStudent, submitQuiz } from "../controllers/studentController.js";
 const router = express.Router();
 
 router.post("/register", registerStudent);
 router.post("/login", loginStudent);
+router.post("/submit-quiz", submitQuiz);
 
 export default router;
