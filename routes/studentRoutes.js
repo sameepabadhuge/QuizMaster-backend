882eba812b80fe683cded46e726ef797067e3592
sameepabadhuge <sithusameepa@gmail.com>
2025-11-27 00:50:15 +0530

    modify

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerStudent, loginStudent, submitQuiz, getLeaderboard } from "../controllers/studentController.js";
+import { registerStudent, loginStudent, submitQuiz, getLeaderboard, getQuizResult } from "../controllers/studentController.js";
 const router = express.Router();
 
 router.post("/register", registerStudent);
 router.post("/login", loginStudent);
 router.post("/submit-quiz", submitQuiz);
 router.get("/leaderboard", getLeaderboard);
+router.get("/quiz-result/:id", getQuizResult);
 
 export default router;
